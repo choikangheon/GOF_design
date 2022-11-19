3ea4d99473a3302a43bf4899231a4cb27db27e45
choikangheon <52992334+choikangheon@users.noreply.github.com>
2022-11-19 23:59:54 +0900

    [chore] 2022-09-22 프로토타입

---
+++ src/main/java/prototype/before/App.java
@@ -0,0 +1,12 @@
+package prototype.before;
+
+public class App {
+
+    public static void main(String[] args) {
+        GithubRepository repository = new GithubRepository();
+
+        repository.setUser("brody");
+        repository.setName("live-study");
+
+    }
+}
---
+++ src/main/java/prototype/before/GithubIssue.java
@@ -0,0 +1,27 @@
+package prototype.before;
+
+public class GithubIssue {
+    private int id;
+    private String title;
+    private GithubRepository githubRepository;
+
+    public GithubIssue(GithubRepository githubRepository) {
+        this.githubRepository = githubRepository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
